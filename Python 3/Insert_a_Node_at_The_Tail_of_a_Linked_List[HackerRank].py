7dacda0cad9e2c96861083be0325f08252056590
Mohammad Rafsun Jany Mahin <59613041+Rafsun-Jany@users.noreply.github.com>
2020-06-27 02:28:21 +0600

    Add files via upload

---
+++ Python 3/Insert_a_Node_at_The_Tail_of_a_Linked_List[HackerRank].py
@@ -0,0 +1,39 @@
+# Complete the insertNodeAtTail function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def insertNodeAtTail(head, data):
+    node = SinglyLinkedListNode(data)
+    if head is None:
+        return node
+    prev = head
+    while True:
+        if prev.next is None:
+            break
+        prev = prev.next
+    prev.next = node
+    return head
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for i in range(llist_count):
+        llist_item = int(input())
+        llist_head = insertNodeAtTail(llist.head, llist_item)
+        llist.head = llist_head
+
+    print_singly_linked_list(llist.head, '\n', fptr)
+    fptr.write('\n')
+
+    fptr.close()
